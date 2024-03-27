cca9d89034976f0902a08712d11e228acbeb2c8a
YanManenti <y.a.martins@unesc.net>
2024-03-27 18:03:19 -0300

    implementando funcionalidades.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("123456789", "Nokia 3310", "123456789", 512);
+Iphone iphone = new Iphone("987654321", "Iphone 12", "987654321", 1024);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
